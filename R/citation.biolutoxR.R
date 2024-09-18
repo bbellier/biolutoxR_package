a177980a9b4ddbd7b1d8dd75b938bdce7aa5dfcc
Benjamin <bbellier.perso@gmail.com>
2024-09-18 10:37:22 +0200

    Edit citation

--- R/citation.biolutoxR.R
+++ R/citation.biolutoxR.R
@@ -1,6 +1,6 @@
 # Fonction qui permet de donner à l'utilisateur la citation de l'article du package
 citation.biolutoxR <- function() {
-  print("To cite this document: Le Picard, C. & Bellier, B. (2024). biolutoxR: An R-Shiny package for easy performing data analysis of a toxicity test based on bacterial bioluminescence inhibition. SoftwareX, XX(XX-XX). DOI. Also, in this site web : https://bbellier.github.io/biolutoxR_website/, you can find all about this package.")
+  print("To cite this document: in prep. Also, you can find all about this package in: https://bbellier.github.io/biolutoxR_website/.")
 }
 
 # Run la function citation.biolutoxR()
