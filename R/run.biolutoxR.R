61fbb2d7ce77795bdd31f6f2fc237455a14d0987
Benjamin <bbellier.perso@gmail.com>
2024-11-03 22:39:14 +0100

    Edit img

--- R/run.biolutoxR.R
+++ R/run.biolutoxR.R
@@ -580,7 +580,7 @@ run.biolutoxR <- function() {
 
     # Valeurs de l'img "img_pckg"
     output$img_pckg <- renderImage({
-      path_to_png_1 <- "www/logo.png"
+      path_to_png_1 <- system.file("www/logo.png", package = "biolutoxR")
       list(src = path_to_png_1,
            width = "92.025",
            height = "105.345")
